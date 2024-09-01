c8e0ad9b41c804111023846a9d81d3efba799384
PratimaYadav22 <143662393+PratimaYadav22@users.noreply.github.com>
2024-09-01 12:36:17 -0500

    Updated CreatePlan.jsx

--- src/Pages/CreatePlan.jsx
+++ src/Pages/CreatePlan.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import { Footer } from "../components/footer";
 import { Search } from "../components/SearchCreatePlan";
-import MapDisplay from "../components/MapDisplay";
-import LocationList from "../components/LocationList";
+import  LocationList  from "../components/LocationList";
+
+
 
 
 const CreatePlan = () => {
   return (
     <div>
       <Search />
-      
-      <div style={{ display: 'flex', height: '78vh' }}>
-            <div style={{ width: '100%' }}>
-                {<div className="flex lg:flex-col lg:flex-row">
-                <LocationList />
-                </div>}
-            </div>
-        <MapDisplay />
-     </div>
-        
+      <LocationList />  
       <Footer />
     </div>
   );
