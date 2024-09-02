42eb88537ac2111f8bf83761619f285d1d712cee
VarshithaPalakonda7 <varshithapalakonda379@gmail.com>
2024-09-02 10:03:53 -0500

    updated Plan Description

---
+++ src/Pages/DetailsCollab.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Footer } from '../components/footer';
+import { Eventheader } from '../components/Eventheader';
+import PlandescriptionCollab from '../components/PlandescriptionCollaborator';
+
+
+
+const DetailsCollab = () => {
+    return (
+        <>
+          <div>
+            <Eventheader />
+            <PlandescriptionCollab />
+            <Footer />
+          </div>
+        </>
+      );
+}
+
+export default DetailsCollab
+
+
